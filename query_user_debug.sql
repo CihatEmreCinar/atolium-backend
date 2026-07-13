07d6465269a97caa27b9e51bd357fb21ce97e029
CihatEmreCinar <cihatemre381@gmail.com>
2026-07-13 22:08:57 +0300

    chore: geçici debug/test script'leri repodan kaldırıldı

--- query_user_debug.sql
+++
@@ -1,9 +0,0 @@
--- Check User record
-SELECT "Id", "Email", "Role", "IsActive", "DeletedAt", "CreatedAt" 
-FROM "Users" 
-WHERE "Id" = 'abc8ea60-1357-4cfe-ab1a-8420994507a6';
-
--- Check EmployerProfile record
-SELECT "Id", "UserId", "WorkshopTitle", "CreatedAt" 
-FROM "EmployerProfiles" 
-WHERE "UserId" = 'abc8ea60-1357-4cfe-ab1a-8420994507a6';
